d232d37201dbd9e54a8eceedc67d5e669fc18f2c
PrasathRep <prasath.g@changepond.com>
2021-09-22 22:18:35 +0530

    Second Try

---
+++ Telent/src/Demo/SOLO.java
@@ -0,0 +1,17 @@
+package Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class SOLO {
+
+	WebDriver driver =null;
+	
+	
+	@Test
+	public void login() {
+		
+		
+		System.out.println("-----------Test outcome------------");
+	}
+}
